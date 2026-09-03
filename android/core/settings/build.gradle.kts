28acf036e8cb2184f1c332c6af2a785d07735321
vitalya482-glitch <vitalya482@gmail.com>
2026-09-03 16:00:11 +0500

    Add core settings module

---
+++ android/core/settings/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    alias(libs.plugins.android.library)
+}
+
+android {
+    namespace = "kz.lvk.languagelearning.core.settings"
+    compileSdk = 37
+
+    defaultConfig {
+        minSdk = 26
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+dependencies {
+    implementation(libs.kotlinx.coroutines.android)
+}
