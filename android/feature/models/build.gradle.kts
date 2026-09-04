9772a84fe9e73f35f2517a7ac48bc528fb575b95
vitalya482-glitch <vitalya482@gmail.com>
2026-09-04 11:57:38 +0500

    Add local models feature module

---
+++ android/feature/models/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.compose)
+}
+
+android {
+    namespace = "kz.lvk.languagelearning.feature.models"
+    compileSdk = 37
+
+    defaultConfig {
+        minSdk = 26
+    }
+
+    buildFeatures {
+        compose = true
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+dependencies {
+    implementation(project(":android:core:models"))
+    implementation(project(":android:core:designsystem"))
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.ui)
+}
